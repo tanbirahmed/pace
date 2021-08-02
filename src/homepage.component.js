cb9c97f24577e4d8f210cfe49e621422a1814afa
Tanbir Ahmed <tanbir.ahmed02@outlook.com>
2021-08-02 08:43:02 -0400

    flexbox grid created

--- src/homepage.component.js
+++ src/homepage.component.js
@@ -1,6 +1,6 @@
 import React from "react";
 const HomePage = () => (
-  <div classNam="homepage">
+  <div className="homepage">
     <div className="directory-menu">
       <div className="menu-item">
         <div className="content">
@@ -13,8 +13,32 @@ const HomePage = () => (
           <h1 className="title">T-Shirts</h1>
           <span className="subtitle">SHOP NOW</span>
         </div>
+        <div className="content">
+          <h1 className="title">T-Shirts</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+        <div className="content">
+          <h1 className="title">T-Shirts</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+        <div className="content">
+          <h1 className="title">T-Shirts</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
       </div>
       <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Business Cards</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+        <div className="content">
+          <h1 className="title">Business Cards</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+        <div className="content">
+          <h1 className="title">Business Cards</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
         <div className="content">
           <h1 className="title">Business Cards</h1>
           <span className="subtitle">SHOP NOW</span>
@@ -26,10 +50,22 @@ const HomePage = () => (
           <span className="subtitle">SHOP NOW</span>
         </div>
       </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">Mugs</h1>
-          <span className="subtitle">SHOP NOW</span>
+      <div className="menu-item1">
+        <div className="content1">
+          <h1 className="title1">Test</h1>
+          <span className="subtitle1">SHOP NOW</span>
+        </div>
+        <div className="content1">
+          <h1 className="title1">Test</h1>
+          <span className="subtitle1">SHOP NOW</span>
+        </div>
+        <div className="content1">
+          <h1 className="title1">Test</h1>
+          <span className="subtitle1">SHOP NOW</span>
+        </div>
+        <div className="content1">
+          <h1 className="title1">Test</h1>
+          <span className="subtitle1">SHOP NOW</span>
         </div>
       </div>
     </div>
